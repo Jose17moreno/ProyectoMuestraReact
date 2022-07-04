c81c5fa8be35e4a577353869017d7ad713997fdc
Jose17moreno <jpm221288@gmail.com>
2022-07-03 22:29:23 -0300

    Subido

---
+++ src/MovieCard.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import styles from "./MovieCard.module.css";
+
+export function MovieCard({ movie }) {
+    const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+    return (
+        <li className={styles.movieCard}>
+            <Link to= {"/movies/" + movie.id}>
+                <img
+                    width={200}
+                    height={350}
+                    className={styles.movieImage}
+                    src={imageUrl}
+                    alt={movie.title}
+                />
+                <div>{movie.title}</div>
+            </Link> 
+        </li>
+    );
+}
---
+++ src/MoviesGrid.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { MovieCard } from "./MovieCard"; 
+import styles from "./MoviesGrid.module.css";
+import { get } from "./utils/httpClient";
+
+export function MoviesGrid() {
+    const [movies, setMovies] = useState([]);
+
+    
+    useEffect(() => {
+        get("/discover/movie").then((data) => {
+            setMovies(data.results);
+        });
+    }, []); 
+    return (
+        <ul className={styles.moviesGrid}>
+            {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+        </ul>
+    );
+}
